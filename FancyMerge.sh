cf2f32fa95d5cad1b5203e0f21ff53b9c2d6dde3
David Golden <dagolden@cpan.org>
2014-10-22 15:43:05 -0400

    check that we start in a git repo

--- FancyMerge.sh
+++ FancyMerge.sh
@@ -1,6 +1,11 @@
 # Initialize
 
+# Make sure current dir is a git repo
+(git rev-parse --is-inside-work-tree &> /dev/null) || echo "Not a git repo" && exit 1;
+
 # Stash old work
+
+# Fetch PR to local
 
 # Move to working branch
 
